81c931a623adcb7b2b8a290ceaf4d2aeb4d4f9fd
maxime <alise.maxime@gmail.com>
2024-01-31 10:16:39 +0100

    minor updates

--- README.md
+++ README.md
@@ -4,11 +4,10 @@
 
 A singleton is an object that has only a single instance throughout the entire application's lifespan.
 
-Implement the getInstance() methods in each of the classes and do any necessary modifications if need be to make the
-class thread-safe and let the unit tests pass :
+Implement the getInstance() methods in each of the classes and do any necessary modifications if need be to make the class thread-safe and let the unit tests pass :
 
 - [A_EagerInitSingleton](A_EagerInitSingleton.java)
-- [B_LazySingleton.java](B_LazySingleton.java)]
+- [B_LazySingleton.java](B_LazySingleton.java)****
 - [C_LazyDoubleCheckedSingleton.java](C_LazyDoubleCheckedSingleton.java)
 - [D_StaticHolderSingleton.java](D_StaticHolderSingleton.java)
 - [E_EnumSingleton.java](E_EnumSingleton.java)
--- src/main/java/com/novencia/jconcurrency/part1/A_CreatingAThread.java
+++ src/main/java/com/novencia/jconcurrency/part1/A_CreatingAThread.java
@@ -1,22 +1,34 @@
 package com.novencia.jconcurrency.part1;
 
+import java.util.concurrent.locks.LockSupport;
+
 public class A_CreatingAThread {
 
     public static void main(String[] args) {
         // passing a runnable
-        var t1 = new Thread(() -> System.out.println(""));
+        var t1 = new Thread(() -> System.out.println("Hello from thread " + Thread.currentThread()));
         t1.start();
 
         // extending Thread
         var t2 = new MyThread();
         t2.start();
 
+        // 21
+        var t3 = Thread.ofPlatform()
+                .name("t3")
+                .start(() -> System.out.println("Hello from thread " + Thread.currentThread()));
+
+        var t4 = Thread.ofVirtual()
+                .name("t4")
+                .start(() -> System.out.println("Hello from thread " + Thread.currentThread()));
+
+        LockSupport.parkNanos(1_000_000_000);
     }
 
     static class MyThread extends Thread {
         @Override
         public void run() {
-            System.out.println("");
+            System.out.println("Hello from thread " + Thread.currentThread());
         }
     }
 }
